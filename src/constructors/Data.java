5f9cba679f6201ba03a15d38661984a4890e1632
Rodney Rizo <rodneyrizo@outlook.com>
2021-11-12 10:31:20 -0600

    excelsior

---
+++ src/constructors/Data.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructors;
+
+public class Data {
+    int id;
+    Double presupuesto;
+
+    public Double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(Double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
